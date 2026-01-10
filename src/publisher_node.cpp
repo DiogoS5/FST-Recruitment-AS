049d40d6b28ca6538a6652fe08c5cd5d27149ebb
DiogoS5 <diogomppsousa@gmail.com>
2026-01-10 18:55:06 +0000

    Publisher input improvement

--- src/publisher_node.cpp
+++ src/publisher_node.cpp
@@ -1,5 +1,7 @@
 #include "ros/ros.h"
 #include "std_msgs/Int32.h"
+#include "std_msgs/String.h"
+#include <string>
 
 int main(int argc, char **argv) {
     ros::init(argc, argv, "Publisher");
@@ -8,18 +10,26 @@ int main(int argc, char **argv) {
     ros::Publisher test_pub;
     test_pub = n.advertise<std_msgs::Int32>("test", 1000);
 
-    ros::Rate loop_rate(1);
-
     while(ros::ok()) {
         std_msgs::Int32 msg;
-        if(!(std::cin >> msg.data)){
-            std::cout << "Please enter a valid integer\n";
-        };
+        std::string input;
 
-        test_pub.publish(msg);
+        std::cout << "Enter a number (or q to quit): ";
+        std::cin >> input;
+
+        if (input == "q") {
+            ros::shutdown();
+            break; 
+        }
+        
+        try {
+            msg.data = std::stoi(input);
+            test_pub.publish(msg);
+        } catch (const std::invalid_argument& e) {
+            std::cout << "Invalid input\n";
+        }
 
         ros::spinOnce();
-        loop_rate.sleep();
     }
 
     return 0;
