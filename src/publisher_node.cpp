295d9c317b1619317f795bd41b1d13b3effe2cde
DiogoS5 <diogomppsousa@gmail.com>
2026-01-10 17:52:15 +0000

    Publish predefined number

---
+++ src/publisher_node.cpp
@@ -0,0 +1,24 @@
+#include "ros/ros.h"
+#include "std_msgs/Int32.h"
+
+int main(int argc, char **argv) {
+    ros::init(argc, argv, "Publisher");
+    ros::NodeHandle n;
+
+    ros::Publisher test_pub;
+    test_pub = n.advertise<std_msgs::Int32>("test", 1000);
+
+    ros::Rate loop_rate(1);
+
+    while(ros::ok()) {
+        std_msgs::Int32 msg;
+        msg.data = 7;
+
+        test_pub.publish(msg);
+
+        ros::spinOnce();
+        loop_rate.sleep();
+    }
+
+    return 0;
+}
